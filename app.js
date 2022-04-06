5d4beb14078ffd60b55d0907e8f9b311d6709635
Anand <anandom2002@gmail.com>
2022-04-06 14:10:02 +0530

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express=require('express')
 const  path=require('path')
 
 const app=express();
-const port = 80;
+const port =process.env.PORT || 80;
 
 //for serving static files
 // app.use(express.static(path.join(__dirname, 'static'));
